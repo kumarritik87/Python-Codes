5ae7f1e0b50424a4968203284ed89d3666306628
Ritik Kumar <ritik.kumar_cs18@gla.ac.in>
2019-10-09 06:21:19 -1200

    Update dicerooling.py

--- dicerooling.py
+++ dicerooling.py
@@ -1,8 +1,6 @@
-""" @ Author:- Ritik Kumar"""
-
-import random
+import random   
 repeat = True
 while repeat:
-    print("you got",random.randint(1,6))
-    print("do you want to rolled again y/n")
-    repeat = "y" in input()
+    print("you got",random.randint(1,6))         
+    print("do you want to rolled again y/n")     
+    repeat = "y" in input()                   
